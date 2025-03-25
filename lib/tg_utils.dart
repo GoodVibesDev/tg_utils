4c6960983710e43da796e4aa6e466807c8f4edaf
Evgeniy.Vabishchevich <evgeniy.vabishevich.99@mail.ru>
2025-03-25 22:57:29 +0300

    added utils file to export

--- lib/tg_utils.dart
+++ lib/tg_utils.dart
@@ -3,3 +3,4 @@ export 'src/exceptions.dart';
 export 'src/inner_message_sender_cache.dart';
 export 'src/model/model.dart';
 export 'src/tele_dart_message_sender.dart';
+export 'src/tg_buttons_utils.dart';
